76ce2dafa638024bd765c0e1d6d9b2f5d65ac1ad
Ed-teer <e-ter@o2.pl>
2026-02-05 12:02:45 +0100

    Refactor Firebase config for export and remove init

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
+// tylko konfiguracja + export (BEZ initializeApp)
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAIgTkQSTCGqyS200Vy-5dwQbgiVK2XThc",
   authDomain: "bila-28483.firebaseapp.com",
   projectId: "bila-28483",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:919635473725:web:4980250d3eebee8f064154"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// jedno ID turnieju (stałe)
+export const TOURNAMENT_ID = "main";
